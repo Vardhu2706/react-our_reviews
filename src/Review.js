81b6f08a960767fd26f34a3709c0f7137f9c12e0
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-04-29 08:22:19 +0530

    Initial project setup.

---
+++ src/Review.js
@@ -0,0 +1,14 @@
+// Importing Helpers
+import React, { useState } from "react";
+
+// Importing Components/Files
+import people from "./data";
+import { FaChevronLeft, FavChevronRight, FaQuoteRight } from "react-icons/fa";
+
+// Functional Component
+const Review = () => {
+  return <h2>Review Component</h2>;
+};
+
+// Export Default
+export default Review;
